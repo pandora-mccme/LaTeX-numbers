0e0c6d93f1c3c1af31342f7cf96f2dcabe3d6976
Vitalii Guzeev <viviag@yandex.ru>
2019-12-04 08:47:33 +0300

    Do not intersperse fractional parts

--- src/LaTeX/Replacement/Rules.hs
+++ src/LaTeX/Replacement/Rules.hs
@@ -36,7 +36,7 @@ fractionalRep = Replacement
 
 integerRep :: ReplacementData
 integerRep = Replacement
-  [re|(\d[\d~]*\d|\d)(\\%)?|] func
+  [re|(?<!\{,\})(\d[\d~]*\d|\d)(\\%)?|] func
     where
       func (s1:s2:_) = addNumericSpaces s1 <> s2
       func (s1:_) = addNumericSpaces s1
